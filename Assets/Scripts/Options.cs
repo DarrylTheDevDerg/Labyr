320fae8cb1827a9a45ac2e526ffede88dd210744
Darryl <pau.pena312@gmail.com>
2024-12-10 09:46:33 -0300

    10-12-24

--- Assets/Scripts/Options.cs
+++ Assets/Scripts/Options.cs
@@ -1,13 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Options : MonoBehaviour
 {
+    public Transform player;
+
+    private AudioListener audioControl;
+    private Volume cam;
+    private int lang;
+    private int postProcess;
+    private float mouseSensibility;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioControl = FindObjectOfType<AudioListener>();
+        cam = FindObjectOfType<Volume>();
+
+        lang = PlayerPrefs.GetInt("Idioma", 0);
+        postProcess = PlayerPrefs.GetInt("Post-procesado", 0);
+        mouseSensibility = PlayerPrefs.GetFloat("Sensibilidad", 200f);
+
+        if (postProcess > 0)
+        {
+            cam.enabled = false;
+        }
+
+        if (player != null)
+        {
+            player.GetComponent<MouseLook>();
+        }
     }
 
     // Update is called once per frame
